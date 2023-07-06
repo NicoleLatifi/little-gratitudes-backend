942eee968103de5d5fa9243822886681d492708f
NicoleLatifi <Nicole.Latifi@gmail.com>
2023-07-06 12:21:08 -0600

    minor lint fixes

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.delete("/gratitudeEntries/:id", async (req, res, next) => {
   }
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   return res.status(err.status || 400).json({
     status: err.status || 400,
     message: err.message || "there was an error processing request",
